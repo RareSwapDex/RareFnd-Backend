7eded4a1b766dfad5736dc34403ee6c053388d06
Jalil <benharkatdjalil@gmail.com>
2022-08-23 01:21:39 +0400

    first commit

--- RareFND/settings.py
+++ RareFND/settings.py
@@ -157,6 +157,18 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+    
+    
+    
+    
+#     'default': {
+#        'ENGINE': 'django.db.backends.postgresql',
+#        'NAME': ‘<database_name>’,
+#        'USER': '<database_username>',
+#        'PASSWORD': '<password>',
+#        'HOST': '<database_hostname_or_ip>',
+#        'PORT': '<database_port>',
+#    }
 }
 
 
